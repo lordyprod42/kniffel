09f4f47a074860450e4edb8327ba38e499f97bf4
lordy_prod <lordy_prod42yahoo.com>
2024-08-09 12:06:24 +0200

    before refactor and translation

--- Kniffel.py
+++ Kniffel.py
@@ -1,15 +1,32 @@
 import random
 
-print ("willkommen zu lordy_prods kniffel")
+print ("""
+       Willkommen zu lordy_prods Kniffel
+    
+    Dies ist mein Kniffel spiel in Python Terminal.
+    Schreibt zahlen wenn diese gefragt sind zum Beispiel
+    wenn gefragt ist welche Würfel man behalten möchte.
+    wird nur 1 2 3 4 5 6 erkannt und einfach ohne freistellen
+    eingeben zum Beispiel '123', behält Würfel 1 2 und 3.
+    Zur frage wo die punkte eingeben werden bitte mit 
+       katogorie der punkte antworten z.B. 
+       'einser' gibt punkte für nur einser, 
+       'full house' gibt punkte für Full House.
 
+       Viel Spaß!!!
 
+       """)
+players = 1
 
+def ask_for_players():
+    spieler = input("wieviele mitspieler gibt es heute?(max 4)(eingeben:1,2,3,4:)")
+    if spieler == "exit":exit()
+    #elif spieler is not range(1,4,1):ask_for_players()
+    if int(spieler) >4 : spieler = "4"
+    print("also: " +spieler + " spielen heute mit.")
+    players = int(spieler)
 
-spieler = input("wieviele mitspieler gibt es heute?(max 4):")
-if spieler == "exit":exit()
-if int(spieler) >4 : spieler = "4"
-print("also: " +spieler + " spielen heute mit.")
-players = int(spieler)
+ask_for_players()
 
 class pointtable:
         
@@ -36,6 +53,7 @@ class pointtable:
 
 
     def print_table(self):
+        print("player: "+str(self.my_player))
         print("nur einser: " +str(self.points_einser))
         print("nur zweier: " +str(self.points_zweier))
         print("nur dreier: " +str(self.points_dreier))
@@ -48,7 +66,7 @@ class pointtable:
         print("kleine straße: " +str(self.kleine_strasse))
         print("große straße: "+str(self.grosse_strasse))
         print("kniffel: " +str(self.kniffel))
-        print("chance: "+str(self.chance))
+        print("chance: "+str(self.chance)+"\n")
 
     def add_teil_1(self):
         self.points_teil_1=self.points_einser+self.points_zweier+self.points_dreier+self.points_vierer+self.points_fünfer+self.points_sechser
@@ -81,7 +99,6 @@ class pointtable:
         self.chance = -1 
 
 player_1_points = pointtable(1)
-player_1_points.print_table()
 player_2_points = pointtable(2)
 player_3_points = pointtable(3)
 player_4_points = pointtable(4)
@@ -122,9 +139,13 @@ def player_turn(player):
     count = 0
     würfel = [1,2,3,4,5]
     würfel_out = []
+    if player == 1:player_1_points.print_table()
+    elif player == 2:player_2_points.print_table()
+    elif player == 3:player_3_points.print_table()
+    elif player == 4:player_4_points.print_table()
     for turn in range(0,3):
         count +=1
-        input12 = input("spieler "+str(player)+" würfeln?")
+        input12 = input("spieler "+str(player)+" würfeln?"+"\n")
         if input12 == "exit":exit()
         throw_dice(würfel)
         print("dein wurf: "+"\n"
@@ -143,42 +164,44 @@ def player_turn(player):
                 if wurf == 4:saved_dice.append(thrown_dice["w4"])
                 if wurf == 5:saved_dice.append(thrown_dice["w5"])
         else:
+            hold = ""
             hold = input("welche würfel halten?"+str(würfel)+":")
         if hold =="exit":exit()
-        print("debug "+str(hold))
+        # checks the input and takes the dice out of the throw dice pool and in the saved dice pool
         for number in hold:
-            if number =="1":
+            if number =="1" and 1 in würfel:
                 die_1.save_die()
                 saved_dice.append(thrown_dice["w1"])
                 würfel.pop(würfel.index(1))
                 würfel_out.append(1)
 
-            if number =="2":
+            if number =="2" and 2 in würfel:
                 die_2.save_die
                 saved_dice.append(thrown_dice["w2"])
                 würfel.pop(würfel.index(2))
                 würfel_out.append(2)
 
-            if number =="3":
+            if number =="3" and 3 in würfel:
                 die_3.save_die()
                 saved_dice.append(thrown_dice["w3"])
                 würfel.pop(würfel.index(3))
                 würfel_out.append(3)
-            if number =="4":
+            if number =="4" and 4 in würfel:
                 die_4.save_die()
                 saved_dice.append(thrown_dice["w4"])
                 würfel.pop(würfel.index(4))
                 würfel_out.append(4)
-            if number =="5":
+            if number =="5" and 5 in würfel:
                 die_5.save_die()
                 saved_dice.append(thrown_dice["w5"])
                 würfel.pop(würfel.index(5))
                 würfel_out.append(5)
         if len(würfel) == 0:break
         if count == 2:
-            input24 = input("weiter?")
+            input24 = input("weiter?"+"\n")
             back=input("welche würfel wieder würfeln?"+str(würfel_out)+":")
             if back =="exit":exit()
+            #reverses the poping of a dice to put it back in the dice pool
             for number in back:
                 if number =="1" and 1 in würfel_out:
                     die_1.unsave_die()
@@ -204,13 +227,11 @@ def player_turn(player):
                     die_5.unsave_die()
                     würfel_out.pop(würfel_out.index(5))
                     würfel.append(5)
-                    print(die_5.saved,würfel)
                     saved_dice.pop(saved_dice.index(die_5.eyes))
-            print("debug: "+str(saved_dice))
 
 
 
-    input23=input("weiter?")
+    input23=input("weiter?"+"\n")
     score_points(player)
 
 def score_points(player):
@@ -293,35 +314,35 @@ def get_points(player,wich):
     elif wich == 33:
         if table.dreier_pasch != -1: score_points(player)
         else:
-            tempbool = false
+            tempbool = False
             for dice in saved_dice:
              if saved_dice.count(dice) >=3:
-                tempbool = true
+                tempbool = True
                 x = 0
                 for point in saved_dice:
                     x += point
                 table.dreier_pasch = x
-            if tempbool == false:
+            if tempbool == False:
                 input7 = input("du hast kein dreier pasch, willst du 0 punkte, schreib 0:")
                 if input7 == "0": table.dreier_pasch =0
                 else:score_points(player)
-            else: tempbool = false
+            else: tempbool = False
     elif wich == 44:
         if table.vierer_pasch != -1: score_points(player)
         else:
-            tempbool = false
+            tempbool = False
             for dice in saved_dice:
              if saved_dice.count(dice) >=4:
-                tempbool = true
+                tempbool = True
                 x = 0
                 for point in saved_dice:
                     x += point
                 table.vierer_pasch = x
-            if tempbool == false:
+            if tempbool == False:
                 input7 = input("du hast kein vierer pasch, willst du 0 punkte, schreib 0:")
                 if input7 == "0": table.vierer_pasch =0
                 else:score_points(player)
-            else: tempbool = false
+            else: tempbool = False
     elif wich == 25:
         if table.full_house != -1: score_points(player)
         else:
@@ -359,17 +380,17 @@ def get_points(player,wich):
         if table.kniffel != -1: score_points(player)
         else:
             x = 0
-            nose = true
+            nose = True
             for dice in saved_dice:
                 if x == 0:
                     x=dice
                 if x != dice:
-                    nose = false
+                    nose = False
                     break
             if nose:
                 table.kniffel = 50
             else:
-                nose = true
+                nose = True
                 input10 = input("du hast kein kniffel willst du 0 punkte, schreib 0:")
                 if input10 == 0: table.kniffel = 0
                 else:score_points(player)
@@ -382,10 +403,16 @@ def get_points(player,wich):
                 x += point
             table.chance = x
     table.print_table()
-    sanother_input = input("weiter?")
+    sanother_input = input("weiter?"+"\n")
+    
     saved_dice.clear()
+    die_1.unsave_die()
+    die_2.unsave_die()
+    die_3.unsave_die()
+    die_4.unsave_die()
     thrown_dice = {"w1":0,"w2":0,"w3":0,"w4":0,"w5":0}
 
+# throws dice and returns thrown eyes
 def throw_dice(würfel):
         if 1 in würfel:thrown_dice["w1"] = die_1.roll_die()
         if 2 in würfel:thrown_dice["w2"] = die_2.roll_die()
@@ -394,14 +421,15 @@ def throw_dice(würfel):
         if 5 in würfel:thrown_dice["w5"] = die_5.roll_die()
         return thrown_dice
 
+#runs the game as a loop where each player gets a turn per round
 def start_game():
-
-    rounds = 1
+    rounds = 14
     for round in range (0,rounds,1):
-        for player in range(1,players+1,1):
-            player_turn(player)
+        for pl in range(0,players+1,1):
+            player_turn(pl+1)
     end_game()
-    
+
+#at the end of the game adds up the players scores
 def end_game():
     points_pl_1=0
     points_pl_2=0
@@ -437,6 +465,10 @@ def end_game():
     print("player 2: "+str(points_pl_2))
     print("player 3: "+str(points_pl_3))   
     print("player 4: "+str(points_pl_4))
+    if points_pl_1 > points_pl_2 and points_pl_1>points_pl_3 and points_pl_1>points_pl_4:print("!!!Player 1 wins!!!")
+    if points_pl_2 > points_pl_1 and points_pl_2>points_pl_3 and points_pl_2>points_pl_4:print("!!!Player 2 wins!!!")
+    if points_pl_3 > points_pl_2 and points_pl_3>points_pl_1 and points_pl_3>points_pl_4:print("!!!Player 3 wins!!!")
+    if points_pl_4 > points_pl_2 and points_pl_4>points_pl_3 and points_pl_4>points_pl_1:print("!!!Player 4 wins!!!")
     input25 = input("neue runde?(ja/nein)")
     if input25 =="ja":start_game()
     else: exit()
